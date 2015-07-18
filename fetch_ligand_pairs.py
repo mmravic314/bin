415011231e4f8ca4dd73422394e0ded1d94b9e24
Marco Mravic <mmravic@mmravic.local>
2015-07-17 18:19:24 -0700

    fetch-ligand-pairs.py

---
+++ fetch_ligand_pairs.py
@@ -0,0 +1,7 @@
+### Marco Mravic UCSF DeGrado Lab July 2015 
+
+## Heavy script that reads in a directory of pdb files with ligands
+#  Blasts the sequence of each protein and find all pdb entries with 100% identity
+#  Downloads them all and outputs a list grouping pdb entries
+
+# looking to find pairs/groups of structures with the same sequence: w/ ligand & apo  
