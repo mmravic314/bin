1114c1c1faa28deff7856195941b62084561cfd3
Marco Mravic <Marco.mravic@ucsf.edu>
2015-07-22 10:10:35 -0700

    fetch_ligand_pairs.py

--- fetch_ligand_pairs.py
+++ fetch_ligand_pairs.py
@@ -5,3 +5,29 @@
 #  Downloads them all and outputs a list grouping pdb entries
 
 # looking to find pairs/groups of structures with the same sequence: w/ ligand & apo  
+
+
+import sys,os
+
+
+##
+# Read .csv file and print list of eligible pdbs
+pdbList = []
+bioLigandF = open( sys.argv[1], 'r')
+for i in bioLigandF:
+
+	#Ignore peptide and nucleic acid ligands
+	line = i.split(',')
+	if line[4].strip('\"') == 'III' or line[4].strip('\"') == 'NUC':
+		continue
+
+	if line[1].strip('\"')  not in pdbList:
+		pdbList.append( line[1].strip('\"') )
+		print line[1].strip('\"'), line
+
+
+
+
+
+
+
