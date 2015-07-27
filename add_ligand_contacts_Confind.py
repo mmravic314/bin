dda47c7b03e5ddccff2651d6c37bf9cf7d11b6c3
mmravic <marco.mravic@ucsf.edu>
2015-07-27 13:54:21 -0700

    update of confind ligands prelim complete

--- add_ligand_contacts_Confind.py
+++ add_ligand_contacts_Confind.py
@@ -65,22 +65,54 @@ def parseLigandStr( lig ):
 
 class Ligand():
 	def __init__(self, serial, coords ):
-		self.name = serial
-		self.coords = coords
-		contacts = []
+		self.name = 	str( serial )
+		self.coords = 	coords
+		contacts = 		[]
 
 	def __repr__(self):
 		return self.name
 
 class Residue():
-	def __init__(self, serial, coords ):
-		self.name = serial
-		self.coords = coords
-		contacts = []
+	def __init__(self, chain, number, identity):
+		self.chain = 		chain 
+		self.number = 		number
+		self.identity = 	identity
+		# Contain each rotamer as list (value) hashed within each amino acid type
+		self.rotamers = 	{} 
 
+		# key should be serial of ligand, value should be a len 20 array w/ number of rotamers
+		# contacting ligand per possible identity
+		self. frequency = 0  
+	
 	def __repr__(self):
-		return self.name
+		return self.chain + ',' + self.number
 
+	#Calculate frequency from number of rotamer contacting ligand
+	def crunch_freq(self):
+
+
+
+					#Check if this rotamer is within 3 Ang from each ligand
+					#contR = Contacts( rotamer )
+					#for l in ligands:
+					#	print l, 
+					#	try: 
+					#		print len( contR.select(3, l.coords) )
+					#	except TypeError:
+					#		print 0
+
+
+					#
+
+		return 0
+
+########### Dictionary for amino acid frequency in data base taking from CONFIND.cpp July 24, 2015
+## should alter these values to reflect database, maybe supply equivilant hash in pickle 
+aaProp = {}
+aaProp["ALA"] = 7.73; aaProp["CYS"] = 1.84; aaProp["ASP"] = 5.82; aaProp["GLU"] = 6.61; aaProp["PHE"] = 4.05;
+aaProp["GLY"] = 7.11; aaProp["HIS"] = 2.35; aaProp["HSD"] = 2.35; aaProp["ILE"] = 5.66; aaProp["LYS"] = 6.27;
+aaProp["LEU"] = 8.83; aaProp["MET"] = 2.08; aaProp["ASN"] = 4.50; aaProp["PRO"] = 4.52; aaProp["GLN"] = 3.94;
+aaProp["ARG"] = 5.03; aaProp["SER"] = 6.13; aaProp["THR"] = 5.53; aaProp["VAL"] = 6.91; aaProp["TRP"] = 1.51; aaProp["TYR"] = 3.54;
 
 
 ########################  MAIN  #####################################
@@ -94,27 +126,36 @@ freqcu 	= args.freqcut
 cmap 	= args.c
 ligStr 	= args.lig
 ligStr 	= parseLigandStr( ligStr )
-print
+print "Entering input PDB"
 
 ### Parse Pdb, using ProDy package, and get input ligand coordinates
 ##  make list of residues (chain,resnum) with heavy atoms within 7 angstroms
 pdbGroup = parsePDB( pdbf )
-print "\n"
 
-contactList = []
+
+contactList = []		# joint array for any residues within 7 angstroms from any ligand heavy atom
+ligands = []			# array of ligand objects (coords + serial)
+unique = []				# Lazy way to recall & index residues "nearby" ligands
 for i in ligStr:
 	lig_coords = pdbGroup.getBySerial( i, i+1 ).getCoords()[0]
+	ligands.append( Ligand( i, lig_coords ) )
 
 	if len( lig_coords ) < 3:
-		print "\nWARNING: No valid coordinates of ligand of serial number", i
+		print "\nWARNING: No valid coordinates of ligand with serial number", i
 
-
+	
 	cont = Contacts( pdbGroup )
+	# Store all residues (as objects) within rcut (default: 7 Angstroms) from each ligand 
 	for j in cont.select( rcut , lig_coords ):
 
-		ID = ( j.getChid() , str( j.getResnum())  )
-		if ID not in contactList:
-			contactList.append( ID )
+		ID = j.getChid() + ',' + str( j.getResnum() ) 
+		
+		#Only consider natural amino acids (optional?) 
+		if j.getResname() not in aaProp.keys(): continue
+
+		if ID not in unique: 
+			unique.append(ID)
+			contactList.append( Residue( j.getChid(), str( j.getResnum()), j.getResname() ) )
 
 
 ########### temp section for handling rotamer library
@@ -135,41 +176,32 @@ for i in ligStr:
 #
 #pic.dump( rotVals, open('/home/xray/termanal/support.default/rotlib/rotVals.pkl', 'wb') )
 
-########### Dictionary for amino acid frequency in data base taking from CONFIND.cpp July 24, 2015
-## should alter these values to reflect database, maybe supply equivilant hash in pickle 
-aaProp = {}
-aaProp["ALA"] = 7.73; aaProp["CYS"] = 1.84; aaProp["ASP"] = 5.82; aaProp["GLU"] = 6.61; aaProp["PHE"] = 4.05;
-aaProp["GLY"] = 7.11; aaProp["HIS"] = 2.35; aaProp["HSD"] = 2.35; aaProp["ILE"] = 5.66; aaProp["LYS"] = 6.27;
-aaProp["LEU"] = 8.83; aaProp["MET"] = 2.08; aaProp["ASN"] = 4.50; aaProp["PRO"] = 4.52; aaProp["GLN"] = 3.94;
-aaProp["ARG"] = 5.03; aaProp["SER"] = 6.13; aaProp["THR"] = 5.53; aaProp["VAL"] = 6.91; aaProp["TRP"] = 1.51; aaProp["TYR"] = 3.54;
-
 
 ### See fraction of potential rotamers of residues nearby ligand reach within 3 Angstroms
-
+print "\nRotamers file"
 with open( routf ) as file:
 
 	record_lines = []
 	stream_path = './tmp'
 	# Write a tmp file for each rotamer to parse w/ prody
+
 	# Call Auxillary pickle dictionary looking up rotamer library info/probailities 
 	rotVals = pic.load( open('/home/xray/termanal/support.default/rotlib/rotVals.pkl', 'rb' ))
+
 	# And another to read amino acid propensities
 
-
 	contactFlag = 0
-	for i in file:
 
-		#Reset frequency count for each chain,res  
-		prvResID = ''
+	# store all non-clashing rotamers of Residues in potential contact with ligand atoms 
+	for i in file:
 
 		# if end of rotamer model, build rotamer object and refresh line list
 		if i[:3] == 'REM': 
-			resID =  tuple( i.split()[1].split(',')[:2] )
-			res = i.split()[1].split(',')[2]
+			
+			resID =  ','.join( i.split()[1].split(',')[:2] )			
 			rotID = i.split()[1].split(',')[2] + ',' +  i.split()[-1] 
-
 			# record rotamer model if in ligand contact list
-			if resID in contactList:
+			if resID in unique:
 				contactFlag = 1
 				tempStream = open( stream_path , 'w' )
 				tempStream.write( i )
@@ -184,28 +216,45 @@ with open( routf ) as file:
 					tempStream.close()
 
 					# build rotamer model
-					print resID, rotID, 'rotamer probability:', rotVals[ rotID ], 'amino acid frq:', aaProp[res]
-
 					rotamer = parsePDB( stream_path )
-
-
-					#
-
+					# Find index of this residue's ID (e.g "A,56"; chain,resNumber) corresponding to Residue object in contactList
+					# Store this rotamer AtomGroup (ProDy  object) in Residue object's hash by its rotamer ID (e.g. "ARG,11")
+					contactList[ unique.index( resID ) ].rotamers[ rotID ] = rotamer
 				
 				else: 
 					tempStream.write( i )
 
-
-
-
 			else: continue
 
+print "\nDone parsing rotamers and pdb\nCalculating frequency of rotamers contacting ligands...\n"
+# For each residue i, start a frequency sum for each ligand atom; checking if if each rotamer is within 3 Angstroms
+# r_i 				= 	potential rotamer of i
+# lig_j 			= 	ligand of atom serial number j
+# P( a ) 			= 	Probability (in percent) of amino acid of that identity, "a", in dataset
+# p( r_i ) 			= 	Probability of rotamer in Lovell/Richardson rotamer library
+# C( r_i, lig_j ) 	= 	binary indicator if any heady atom of that rotamer is within 3 Ang of ligand j
+################ Frequency =   sum( sum[ C(r_i, lig_j)*p(r_i)*P(a) ]-over-all-r_i-of-a )-over-all-a  /  sum( sum[ p(r_i)*P(a) ]-over-all-r_i-of-a )-over-all-a
+#####################################################################
+for resi in contactList:
+	for l in ligands:
+		fr 		= 0
+		maxV 	= 0
+		for name , rotObj in resi.rotamers.items():		
+			# Increase max possible count
+			maxV += rotVals[ name ] * aaProp[ name.split(',')[0] ]
+			# Check if there a contact between current rotamer atomGroup object, r_i and current ligand, lig_j
+			# pass if not, count it if so
+			contR = Contacts( rotObj )
+			try :
+				if len( contR.select(3, l.coords) ) > 0:
+					fr += rotVals[ name ] * aaProp[ name.split(',')[0] ]
+			except TypeError:
+				pass		 
+
+		print maxV, resi, resi.identity, fr, l, round( fr/maxV, 5 ) 
 
 
-
-			#sys.exit()
-
-
+#print r, k, 'rotamer probability:', rotVals[ k ], 'amino acid frq:', aaProp[ k.split(',')[0] ]
 
 
 
