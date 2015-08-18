4c7e77f1966a4f97991bbad9d0c02ffa6ce04786
mmravic <marco.mravic@ucsf.edu>
2015-08-18 12:16:15 -0700

    finished Zn cleaning

--- PDButil.py
+++ PDButil.py
@@ -40,50 +40,95 @@ natAA["ARG"] = 'R'; natAA["SER"] = 'S'; natAA["THR"] = 'T'; natAA["VAL"] = 'V'; 
 ############### Class def's ##############
 
 
-# mapped PDB chain containing which pdb residue corresponds to each uniprot entry from DBREF lines 
-# Also holds uniprot with download option
+# mapped PDB chain containing which pdb residue corresponds to each uniprot entry 
+#Has optiono to initialize or update a chain with a DBREF line (pdbfile) or list of segments from EMBL SIFTS mapping .tsv file 
 class PdbChain():
+	
 	# does mapping at initialization given a DBREF line from a PDB file
-	def __init__(self, pdbLine):
-		self.name = pdbLine[7:11] + pdbLine[12].lower() 
-		self.uniprot = pdbLine[33:39]
-		self.pdbRes = np.arange( int( pdbLine[14:18].strip() ) , int ( pdbLine[19:24].strip() ) + 1 )
-		self.uniRes  = np.arange( int( pdbLine[55:60].strip() ) , int ( pdbLine[62:68].strip() ) + 1 )
-		self.pdbMetalCoords = []
+	def __init__(self, init_flg, init_info):
+		self.name 		= ''
+		self.uniprot 	= ''
+		self.pdbRes 	= np.array( [] )
+		self.uniRes  	= np.array( [] )
+		self.exceptFlg 	= False				# This flag is True if the   
+
+		if init_flg == 'pdb':
+			self.pdbLine_init(init_info)	# initialize with DBREF lines
+		elif init_flg == 'SIFTS':
+			self.SIFTS_init(init_info)	# initlize with list of segments in SIFTS .tsv file format
+		else:						#Stay empty
+			pass
+
 
 	def __repr__(self):
 		return '%s, %s' % (self.name, self.uniprot) 
 
-	def pdb2uni(self, resNum):
+	# For each chain in pdb entry, should have uniprot mapping at chain level (except large structures or synthesized constructs)
+	def pdbLine_init(self, pdbLine):
+		self.name = pdbLine[7:11] + pdbLine[12].lower() 
+		self.uniprot = pdbLine[33:39]
+		self.pdbRes = np.arange( int( pdbLine[14:18].strip() ) , int ( pdbLine[19:24].strip() ) + 1 )
+		self.uniRes  = np.arange( int( pdbLine[55:60].strip() ) , int ( pdbLine[62:68].strip() ) + 1 )
+		return
 
+	# reads in format from EMBL-EBI mapping https://www.ebi.ac.uk/pdbe/docs/sifts/quick.html, pdb to uniprot
+	def SIFTS_init(self, lineList):
+		if len(lineList) == 0: print 'WHY', self.name, self.uniprot
+		for i in lineList:
+			ln 			= i.rstrip().split(',')
+			chain 		= ''.join( [ln[0].upper(), ln[1].lower() ] )
+			uniprot 	= ln[2]
+			try:
+				p_res 	= np.arange(  int( ln[5] ) , int( ln[6] ) +1 )
+			except ValueError:
+				p_res 	= np.array( [] )
+				self.exceptFlg = True
+			try:	
+				u_res 		= np.arange(  int( ln[7] ) , int( ln[8] ) +1 )
+			except ValueError:
+				u_res 	= np.array( [] )
+				self.exceptFlg = True
+
+			self.pdbRes = np.append( self.pdbRes, p_res )
+			self.uniRes = np.append( self.uniRes, u_res )
+
+		self.name		= chain
+		self.uniprot 	= uniprot
+		return
+
+
+	def pdb2uni(self, resNum):
+		val = 'X'
 		try:
 			val = list( self.pdbRes ).index( resNum )
 		except ValueError:
 			print "Requested pdb residue %d not in chain %s" % (resNum, self.name)
-			sys.exit()
+			pass
 
 		try:
 			val = self.uniRes[val]
 		except IndexError:
 			print  "Requested pdb residue %d not in uniprot mapping of chain %s" % (val, self.name) 
-			sys.exit()
+			pass
 		
 		return val
 
 
 	def uni2pdb(self, resNum):
-		
+		val = 'X'
 		try:
 			val = list( self.uniRes ).index( resNum )
 		except ValueError:
 			print "Requested uniprot residue %d not in uniprot entry %s" % (resNum, self.uniprot)
-			sys.exit()
+			pass
 
 		try:
 			val = self.pdbRes[val]
 		except IndexError:
 			print  "Requested uniprot residue %d not in pdb mapping of chain %s" % (val, self.name) 
-			sys.exit()
+			pass
 		
 		return val
+
+
 ####################### Class definitions end #####################
--- binuclearCleanPDB.py
+++ binuclearCleanPDB.py
@@ -3,7 +3,7 @@
 # Download all chains once, comment out that section
 # Determine which pdb chains have zinc binding capabilities in nature (despite crystallization metals) from uniprot
 # Create hash table out of chain mappings to compare coordination ligands and UniProt descriptions
-# Output list of PDBs with natural zinc2+ (and Mg2+?) to cull for seq ID
+# Output list of PDBs with natural zinc2+ (and Mg2+/Mn2+?) to cull for seq ID
 ## Details:
 # rEMOVE dna/rna ENTRIES
 # If no uniProt, assume denovo design or catalytic antibody. Gotta hand eval, don't disclude
@@ -20,8 +20,41 @@ import sys, os, numpy as np, cPickle as pic
 
 # Example command line
 # Run from directory containing pickle files (e.g. ~/tertBuilding/)
-# python ~/bin/binuclearCleanPDB.py ./binuclearDivalentPDB.txt ~/pdb_080615 ~/tertBuilding/binucUniProt/
-# python ~/bin/binuclearCleanPDB.py path2PDBchainUniprotList path2PDBfiles path2uniprot
+# python ~/bin/binuclearCleanPDB.py ./binuclearDivalentPDB.txt ~/pdb_080615/ ~/binucUniProt/ ~/pdb_chain_uniprot.csv 
+# python ~/bin/binuclearCleanPDB.py path2PDBchainUniprotList path2PDBfiles path2uniprot path2SIFTS_pdb2uniProt_mapping
+
+## This function creates a hash (saved to cPickle binary file) containing PdbChain objects (see PDButil.py in my local bin)
+def SIFT_mapping(pdbList):
+	SIFTS_pdb_uniP_Map = {}
+
+	with open( sys.argv[4] ) as file:
+		prvCh = ''	
+		segments = []
+		for i in file:
+			if i[:4].upper() not in pdbList:
+				continue
+
+			chId = ''.join( [ i[:4].upper(), i[5].lower() ] )
+
+			if prvCh == '': 
+				segments.append(i)
+				prvCh = chId
+				continue
+
+			# If new chain is reached, initialize & store the previous. Then, begin the next
+			if prvCh != chId:
+				SIFTS_pdb_uniP_Map[ prvCh ] = PdbChain( 'SIFTS', segments )
+				segments = []
+				segments.append(i)
+			#For segments of the same chain, keep adding
+			else:
+				segments.append(i)
+
+			prvCh = chId
+
+	return SIFTS_pdb_uniP_Map
+
+
 
 
 ####### This section commeted out but ran once to get all UniProt entries and save a hash of uniprot-PDB mapping (chain level)
@@ -64,6 +97,7 @@ pdbs 	= pic.load( open(  os.path.abspath( os.path.join('./', 'binucPDB.pkl') ), 
 ## Parse each PDB and UniProt entries to create lists of annotated/validated 'natural' binuclear zincs and less strict di-zinc sites
 ## Save each PDB chain object in hash with metal pair objects detailing contacting residues
 chainObj = {}
+
 
 
 ########## Metal pair class  ##########
@@ -198,18 +232,188 @@ def find_sites(pdbs):
 #pairsByPdb = find_sites( pdbs )
 #pic.dump(pairsByPdb, open( '/home/xray/tertBuilding/biPairs_byPDB.pkl', 'wb') )
 
-# Look for contact to specified elements (str type) annotated in uniprot [between input residues(takes number only)]
-def parseUniprot(path, atom, resList):
-	with open(path) as file:
-		for i in file:
-			#print i.upper()
-			if 'ZN' in i.upper():
-				print i.rstrip()
-
-	return
-
 ### Created this hash in the 'find binuclear sites...' section, which is now commented out
 pairsByPdb = pic.load( open( os.path.abspath( os.path.join('./', 'biPairs_byPDB.pkl') ), 'rb') )
+
+#Create mapping of multinuclear sites (from inMap) OR just load the pre-made pickle
+## PdbChain objects indexed in hash by chain
+#SIFTS_pdb_uniP_Map = SIFT_mapping( pairsByPdb.keys() )
+#pic.dump( SIFTS_pdb_uniP_Map, open( '/home/xray/tertBuilding/SIFTS_mapping.pkl' , 'wb' ) )
+#sys.exit()
+SIFTS_pdb_uniP_Map = pic.load(  open( '/home/xray/tertBuilding/SIFTS_mapping.pkl' , 'rb' ) )
+
+
+
+### Functions for the following section
+# PDB Mapping from pdb residue index to uniprot residue index
+# try proper PDB file, then path to the hash (made above) from EMBL-EBI SIFTS (https://www.ebi.ac.uk/pdbe/docs/sifts/quick.html)
+# Return a list  containing PdbChain objects (see PDButil.py module in my local bin) represented by string '(pdbChain, uniprot)'
+def find_mapping( path2pdb, chain, SIFTS_flg = False):
+	tmp = ''
+	with open(path2pdb) as file:
+		for i in file:
+			if i[:6] == 'DBREF ':
+				tmp = PdbChain( 'pdb', i )
+				if tmp.name == chain:
+					break
+				else:
+					tmp = ''
+
+			if i[:4] == 'ATOM': break
+
+	# SIFTS_flg just forces replacement of PDB-DBREF mapping to SIFTS mapping
+	if tmp != chain or not SIFTS_flg:
+		try: 
+			tmp = SIFTS_pdb_uniP_Map[ chain ] 
+		except KeyError:
+			print "No Valid uniprot - PDB mapping available for", chain
+
+	return tmp
+
+
+# Look for contact to specified elements (str type) annotated in uniprot [between input residues(takes number only)]
+def parseUniprot(path, atom, resList, pChain ):
+	mention = False
+	reviewed = False
+	metal_seen = False
+	add2_DB = False
+	coordinating = []
+	divalents = []
+	#Parse
+	with open(path) as file:
+		for i in file:
+			if i[:2] == 'ID': 
+				if 'Reviewed' in i:
+					reviewed = True
+
+			## If metal lines found and Zinc not mentioned, Turn false positive to false negative
+		
+			if i[0:10] == 'FT   METAL':
+				coordinating.append( i )
+				if 'DIVALENT METAL CATION' in i.upper():
+					if i.split()[2] != i.split()[3]: 	#If string of residues bind, hand this annotation differently
+						span = [ str(x) for x in np.arange( int(i.split()[2]), int( i.split()[3] ) + 1  ) ]
+						divalents.extend(span)
+					else:										#This is normal way to record binding sites in metal binding annotations
+						divalents.append( int( i.split()[2] ) ) 
+
+			if 'ZN' in i.upper() or 'ZINC' in i.upper():
+				#print i.rstrip()
+				mention = True
+
+	#### Evaluate if this should be included in zinc data base 
+				
+	# If entry has not be reviewed, and there is a dizinc site  --> accept as true site
+	if not reviewed:
+		add2_DB = True
+		print "not reviewed"
+		return add2_DB
+
+
+	# If entry has been reviewed but no mention of zinc, and there is a dizinc site  --> accept as true site
+	if reviewed:
+		if len( coordinating ) == 0:
+			add2_DB = True
+			print 'Reviewed, but No coordinating residue info'
+			return add2_DB
+
+		# if reviewed and ZN has residue info, check if enough info and if it matches PDB contacts found previously
+		else:
+			annotatedZn = []
+			non_Zn		= []
+			for ln in coordinating:
+
+				if 'ZN' in ln.upper() or 'ZINC' in ln.upper():
+					if ln.split()[2] != ln.split()[3]: 	#If string of residues bind, hand this annotation differently
+						span = [ x for x in np.arange( int(ln.split()[2]), int( ln.split()[3] ) + 1  ) ]
+						annotatedZn.extend(span)
+					else:										#This is normal way to record binding sites in metal binding annotations
+						annotatedZn.append( int( ln.split()[2] ) ) 
+				
+				else:
+					non_Zn.append( ln )
+
+			# If only Zn annotations in reviewed uniprot, and binuclear Zn present  --> accept as true
+			if len(non_Zn) == 0 and len(annotatedZn) >= 0:
+				add2_DB = True
+				print "Reviewed, only coordinating zincs"
+				return add2_DB
+				#print non_Zn
+
+			# If no Zn, but "divalent metal binding" listed, check if residues of coordinating sites match			
+			if len( divalents ) > 0:
+				# Get residues contacting the metal in question and convert residue numbering to uniprot numbering via mapping function
+
+				metal_cont = [ int( filter(lambda x: x.isdigit(), res.split('-')[0] ) ) for res in resList]
+
+				# Use mapping function to get chain object to do mapping conversion
+				path2pdb = os.path.abspath( os.path.join(sys.argv[2], '%s.pdb' % chain[:4] ) )
+				ChObj = find_mapping( path2pdb, pChain )
+
+
+				#Convert pdb numbering to uniprot residue indices
+				foundSites = []
+				for res in metal_cont:
+					try: 
+						foundSites.append( int( ChObj.pdb2uni( res )) )   
+					except ValueError:
+						pass
+				mtch = 0
+
+				for i in foundSites:
+					if i in divalents:
+						mtch +=1 
+						continue
+				# If >= 80% of observed ZN coordinated residues are listed in vague metal sites, accept as valid ZN site. 
+				correct = float( mtch ) /len(foundSites)
+				if correct >= 0.8:
+					add2_DB = True
+					print 'Reviewed, lists \"divalent metal cation\" and observed ZN sites match these general metal sites'
+					return add2_DB
+
+
+			# If multiple metals, including those residues coordinating Zn, check if residues match
+			if len(non_Zn) > 0 and len(annotatedZn) > 0:
+
+
+				metal_cont = [ int( filter(lambda x: x.isdigit(), res.split('-')[0] ) ) for res in resList]
+
+				# Use mapping function to get chain object to do mapping conversion
+				path2pdb = os.path.abspath( os.path.join(sys.argv[2], '%s.pdb' % chain[:4] ) )
+				ChObj = find_mapping( path2pdb, pChain )
+
+
+				#Convert pdb numbering to uniprot residue indices
+				foundSites = []
+				for res in metal_cont:
+					try: 
+						foundSites.append( int( ChObj.pdb2uni( res )) )   
+					except ValueError:
+						pass
+				mtch = 0
+
+				for i in foundSites:
+					if i in annotatedZn:
+						mtch +=1 
+						continue
+				# If >= 80% of observed ZN coordinated residues are listed in vague metal sites, accept as valid ZN site. 
+				correct = float( mtch ) /len(foundSites)
+				if correct >= 0.8:
+					add2_DB = True
+					print 'Reviewed, lists \"divalent metal cation\" and observed ZN sites match these general metal sites'
+					return add2_DB
+
+
+
+		if not mention:
+			if len(coordinating) == 0:
+				add2_DB = True
+				print "Reviewed, but not mentioned and no annotated metals found"
+				return add2_DB
+
+	print add2_DB
+	return add2_DB
+
 
 
 ############ map coordinating residues to uniprot numbering and see if corresponds to known zinc sites  ############3
@@ -218,45 +422,66 @@ zn_cryst		=	[]
 zn_annotated	=	[]
 zn_dbase		=	[]
 hitchains 		= 	[]
+cnt = 0
 
 from collections import defaultdict
 
+
 for s, c in sorted( pairsByPdb.items() ):
-	print s, c
+#	print s, c
 	# for each binuclear site in the set of pdbs
+	
 	for m in c:
 		a1 = m.name.split('_')[0]
-		a2 = m.name.split('+')[0].split('_')[0]
+		a2 = m.name.split('+')[1].split('_')[0]
+
 		#option 1: both are crystallographic zincs  -> next check if this is confirmed by uniprot/text 	 
 		if a1 == 'ZN' and a2 =='ZN':
-			print m
-
+			
 			if s not in zn_cryst:
+				print s,m
 				zn_cryst.append(s)
 				
 				# One a double zinc containing structure is found, look at its coordinating residues
 				# make sure it's in uniprot as zinc binding
-
 				chProt = defaultdict(list)	#chain/uniprots to check ( saving residues )
 				for res in m.contacts:
+
 					chain = s + res.split('_')[0].lower()
 					chProt[chain].append( res )
 				
-				if len(chProt) == 0 : continue		# no contacts, skip .
+				if len( chProt[chain] ) == 0 : 
+					print 'no contacts to chain'
+					continue		# no contacts, skip .
+
+
 				for u in chProt.keys():
 					try:
 						uPath = os.path.abspath( os.path.join(sys.argv[3], '%s.txt' % inMap[u] ) )
 						print "entering...", uPath 
-						parseUniprot( uPath,  'ZN' , chProt[u])
+						if parseUniprot( uPath,  'ZN' , chProt[u], u ): 
+							if s not in zn_dbase:
+								zn_dbase.append(s)
+								print
+								break
 						print
 					except KeyError:
-						# What to do when there is no uniprot file!!!
+						print 'no uniprot for this chain!!!!!!!!!!!!!!!!!!!!!!!', u
+						if s not in zn_dbase:
+							zn_dbase.append(s)
+							print 
+							break
+			else:
+				break		# quit iterating if pdb qualifies
 
-
+			print
 			#sys.exit()
 
+print '\nTRUE DI-ZINC PDBs FOUND', len( zn_dbase )
+print 'out of potential di-Zinc:', len( zn_cryst )
 
-
+for i in zn_dbase:
+	print i
 
 	#print s, os.path.exists( uniPath)
 	#if os.path.exists( uniPath)
